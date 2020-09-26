ea9f1d83da034dbd64da1cd2c30752a18baea5c0
Jill Royer <hello@jillroyer.me>
2020-09-26 11:43:03 +0200

    feat: also remove featured image from singular video psot

--- web/app/themes/generatepress-child/functions.php
+++ web/app/themes/generatepress-child/functions.php
@@ -10,3 +10,13 @@ add_filter('generate_featured_image_output', function($output){
 
     return $output;
 }, 50);
+
+add_action('wp', function() {
+    if (is_singular() && get_post_format() === 'video') {
+        remove_action('generate_after_entry_header', 'generate_blog_single_featured_image');
+        remove_action('generate_before_content', 'generate_blog_single_featured_image');
+        remove_action('generate_after_header', 'generate_blog_single_featured_image');
+        remove_action('generate_before_content', 'generate_featured_page_header_inside_single');
+        remove_action('generate_after_header', 'generate_featured_page_header');
+    }
+}, 200);
